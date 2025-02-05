b44802f3dc3a88682c044d2afa04b4d5be80ba4d
Joshua Wangia <joshuawangia@gmail.com>
2025-02-05 10:00:27 -0500

    Finished Product with weird sort

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Deck myDeck = new Deck();
+        Hand newHand = new Hand(5, myDeck);
+        System.out.println(newHand.printHand());
+        newHand.sortHand();
+        System.out.println( newHand.printHand());
+
     }
 }
